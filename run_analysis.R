dbbc88567dcb3bb20652ec5f17182fb4964ac3c7
jesusarielpena <jesusarielpena@gmail.com>
2015-07-23 21:12:14 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,11 @@
-## This script reads the data to be analyzed from the working directory  
- 	##and write the final tidy data into the data folder 
+## The following  script reads the data to be analyzed from the current working directory and 
+ ## writes a tidy data set 
  
 
- ## Load needed packages 
+ ## First we need two load the following 2 packages that will be used for this assigment
  library(dplyr) 
  library(tidyr) 
  
-<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
- ## Find working directory and read in files 
- wd <- getwd() 
- wd <- paste(wd, "getdata-projectfiles-UCI HAR Dataset\UCI HAR Dataset", 
-                	sep="") 
- 
-
- ## Read train data 
- fwd <- paste(wd,"/train/",sep="") 
- setwd(fwd) 
- 
- 
- 
- ##setwd("C:/Users/penajes2/Documents/UCI HAR Dataset")
- 
- 
- X_train <- read.table("X_train.txt") 
- y_train <- read.table("y_train.txt") 
- subject_train <- read.table("subject_train.txt") 
- ysub_train <- cbind(subject_train, y_train) 
- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
- 
- 
- ------------------------------------------------
  setwd("C:/Users/penajes2/Documents/UCI HAR Dataset")
  
  
@@ -60,34 +35,7 @@
  features <- tbl_df(features) 
  alabel <- read.table("activity_labels.txt", quote="\"")
  names(alabel) <- c("activity_labels", "activity")  ##to give a  name to alabel columns
-------------------------------------------------------- 
 
-   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
- ## Read test data 
- fwd <- paste(wd,"/test/",sep="") 
- setwd(fwd) 
- X_test <- read.table("X_test.txt") 
- y_test <- read.table("y_test.txt") 
- subject_test <- read.table("subject_test.txt") 
- ysub_test <- cbind(subject_test, y_test) 
- 
- ## Combine X test/train 
- X <- rbind(X_train, X_test) 
- ysub <- rbind(ysub_train, ysub_test) 
- 
-
- ## Convert to dplyr table format 
- X <- tbl_df(X) 
- ysub <- tbl_df(ysub) 
- 
-
- ## Read-in the "feature.txt" and "activity_labels.txt" files  
- setwd(wd) 
- features <- read.table("features.txt") 
- features <- tbl_df(features) 
- alabel <- read.table("activity_labels.txt") 
- names(alabel) <- c("activity_labels", "activity") 
- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
  ## split features to obtain the indices of the required measurement    separa cada actividad 
  featsep <- separate(features, V2, c("activity", "stat", "coord"), sep = "-",  
